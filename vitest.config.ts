3e97d487a93f495bbaaba7b14ff8afd3a9e6c732
Rob Abby <robabby23@gmail.com>
2025-10-17 19:49:52 -0300

    Setup Vitest, RTL and initial Smoke Test

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import path from "node:path";
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "happy-dom",
+    globals: true,
+    include: ["__tests__/**/*.test.{ts,tsx}"],
+    setupFiles: ["./__tests__/setup.ts"],
+    coverage: {
+      reporter: ["text", "lcov"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  esbuild: {
+    jsx: "automatic",
+  },
+});
